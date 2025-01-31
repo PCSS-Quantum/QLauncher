f731c358423c65a0f9c92e922608d49cca3effd4
Dawid64 <dawid.siera@gmail.com>
2025-01-31 12:56:58 +0100

    Added equations module with so far only one_in_n equation

---
+++ quantum_launcher/hampy/equations.py
@@ -0,0 +1,43 @@
+"""
+`equations` module provides additional binary operations for Hampy objects.
+
+It's goal is too simplify the creation of more complex problem implementations, by creating them with use of smaller ones.
+"""
+from typing import Optional
+from .object import HampyEquation, HampyVariable
+
+
+def one_in_n(variables: list[int | HampyVariable], size: Optional[int] = None) -> HampyEquation:
+    """
+    Generates HampyEquation for One in N problem.
+
+    One in N returns True if and only if exactly one of targeted indexes in 1, and all others are 0.
+
+    Args:
+        variables (list[int  |  HampyVariable]): Triggered variables or variable indexes
+        size (Optional[int], optional): Size of problem, if not given it takes the first found HampyVariable.size value. Defaults to None.
+
+    Returns:
+        HampyEquation: HampyEquation with returning True if exactly one of passed indexes is 1, False otherwise
+    """
+    if size is None:
+        for var in variable:
+            if isinstance(var, HampyVariable):
+                size = var.size
+                break
+
+    eq = HampyEquation(size)
+    new_variables = set()
+    for var in variables.copy():
+        if isinstance(var, int):
+            new_variables.add(eq.get_variable(var))
+        elif isinstance(var, HampyVariable):
+            new_variables.add(eq.get_variable(var.index))
+
+    for variable in new_variables:
+        equation = variable
+        for different_var in new_variables - {variable}:
+            equation &= ~different_var
+        eq |= equation
+
+    return eq
