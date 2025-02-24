55e4286a6ff13f777716833c570f6d74298991a3
Dawid64 <dawid.siera@gmail.com>
2025-02-24 13:35:30 +0100

    Added get_translation method

--- quantum_launcher/base/qasm_translator.py
+++ quantum_launcher/base/qasm_translator.py
@@ -1,19 +1,23 @@
-from typing import Any
+from typing import Any, Dict, Type, TypeVar
 from abc import ABC, abstractmethod
 import cirq
 from cirq.contrib.qasm_import import circuit_from_qasm
-from qiskit import QuantumCircuit, qasm2
+import qiskit
+from qiskit import qasm2
+
+Circuit = TypeVar('Circuit')
 
 
 class Translation(ABC):
     """ Translation layer for circuits written in different languages """
-    translation_map = {}
-
-    def __init__(self):
-        pass
+    language: str = 'None'
+    circuit_object: Type = None
+    translation_map: Dict[str, "Translation"] = {}
+    object_map: Dict[Circuit, "Translation"] = {}
 
     def __init_subclass__(cls):
-        Translation.translation_map[cls.__name__] = cls
+        Translation.translation_map[cls.language] = cls()
+        Translation.object_map[cls.circuit_object] = cls()
 
     @abstractmethod
     def to_qasm(self, circuit: Any) -> str:
@@ -23,9 +27,17 @@ class Translation(ABC):
     def from_qasm(self, qasm: str) -> Any:
         pass
 
+    @staticmethod
+    def get_translation(circuit: Circuit, language: str) -> Circuit:
+        circuit_qasm_translator = Translation.object_map[circuit.__class__]
+        qasm_circuit_translator = Translation.translation_map[language]
+        qasm = circuit_qasm_translator.to_qasm(circuit)
+        return qasm_circuit_translator.from_qasm(qasm)
+
 
 class CirqTranslation(Translation):
     language: str = 'cirq'
+    circuit_object: Type = cirq.Circuit
 
     def to_qasm(self, circuit: cirq.Circuit) -> str:
         return circuit.to_qasm()
@@ -36,9 +48,10 @@ class CirqTranslation(Translation):
 
 class QiskitTranslation(Translation):
     language: str = 'qiskit'
+    circuit_object: Type = qiskit.QuantumCircuit
 
-    def to_qasm(self, circuit: QuantumCircuit) -> str:
+    def to_qasm(self, circuit: qiskit.QuantumCircuit) -> str:
         return qasm2.dumps(circuit)
 
-    def from_qasm(self, qasm: str) -> QuantumCircuit:
-        return QuantumCircuit.from_qasm_str(qasm)
+    def from_qasm(self, qasm: str) -> qiskit.QuantumCircuit:
+        return qiskit.QuantumCircuit.from_qasm_str(qasm)
