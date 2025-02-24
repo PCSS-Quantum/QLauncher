10ec2f9c11b1cbe9d518d05b61505cee87eeeba2
Dawid64 <dawid.siera@gmail.com>
2025-02-24 16:20:29 +0100

    Updated translation test

--- tests/launcher/test_translation.py
+++ tests/launcher/test_translation.py
@@ -1,16 +1,24 @@
 from quantum_launcher.base.qasm_translator import QiskitTranslation, CirqTranslation, Translation
-from qiskit import QuantumCircuit
-from cirq import Circuit
+import qiskit
+import cirq
 
 
 def test_qiskit_to_cirq_translation():
-    circuit = QuantumCircuit(2)
+    circuit = qiskit.QuantumCircuit(2)
     circuit.h(0)
     circuit.cx(0, 1)
     circuit.x(0)
-    # translator_to = QiskitTranslation()
-    # translator_from = CirqTranslation()
-    # qasm = translator_to.to_qasm(circuit)
-    # cirq_circuit = translator_from.from_qasm(qasm)
+    translator_to = QiskitTranslation()
+    translator_from = CirqTranslation()
+    qasm = translator_to.to_qasm(circuit)
+    cirq_circuit = translator_from.from_qasm(qasm)
+    assert isinstance(cirq_circuit, cirq.Circuit)
+
+
+def test_auto_translation():
+    circuit = qiskit.QuantumCircuit(2)
+    circuit.h(0)
+    circuit.cx(0, 1)
+    circuit.x(0)
     cirq_circuit = Translation.get_translation(circuit, 'cirq')
-    assert isinstance(cirq_circuit, Circuit)
+    assert isinstance(cirq_circuit, cirq.Circuit)
