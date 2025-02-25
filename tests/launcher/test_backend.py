39396b9040381fa973223b3986e9b95fbfd8bb7d
Jakub Jagla <jakub.jagla01@gmail.com>
2025-02-25 14:15:21 +0100

    add test checking if retrieving an online backend with valid token works

--- tests/launcher/test_backend.py
+++ tests/launcher/test_backend.py
@@ -1,8 +1,23 @@
 from qiskit_aqt_provider.aqt_resource import OfflineSimulatorResource
 from qiskit_aqt_provider.primitives import AQTSampler, AQTEstimator
+from qiskit_aqt_provider import AQTProvider
 
 from quantum_launcher.routines.qiskit_routines.backend import AQTBackend
 import pytest
+
+
+class DummyProvider(AQTProvider):
+    def backends(self, name=None, *, backend_type=None, workspace=None):
+        if backend_type == "device":
+            offline_no_noise = super().backends(name=r".*no_noise", backend_type="offline_simulator", workspace=workspace)[0]
+            offline_no_noise.name = "ibex_dummy"
+            return [offline_no_noise]
+        return super().backends(name, backend_type=backend_type, workspace=workspace)
+
+    def get_backend(self, name=None, *, backend_type=None, workspace=None):
+        offline_no_noise = super().backends(name=r".*no_noise", backend_type="offline_simulator", workspace=workspace)[0]
+        offline_no_noise.name = name
+        return offline_no_noise
 
 
 def test_AQT_backend():
@@ -16,3 +31,11 @@ def test_AQT_backend():
     # Test if backend rejects invalid token for online backend
     with pytest.raises(ValueError):
         backend = AQTBackend(token="test_token", name='backendv1v2_simulator')
+
+    backend = AQTBackend(token="test_token", name='local_simulator')
+
+    backend.provider = DummyProvider()
+    backend.name = 'backendv1v2_simulator'
+    backend._set_primitives_on_backend_name()
+
+    assert backend.name == 'ibex_dummy'
