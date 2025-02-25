601470a4a08cd681c361c06ca9648d9c9d3c31ff
Jakub Jagla <jakub.jagla01@gmail.com>
2025-02-25 10:13:56 +0100

    Modify hampy tutorial and add utility functions to hampy __init__

--- quantum_launcher/hampy/__init__.py
+++ quantum_launcher/hampy/__init__.py
@@ -2,5 +2,6 @@
 from .object import Equation, Variable
 from .equations import one_in_n
 from .debug import TruthTable
+from .utils import shift_affected_qubits
 
-__all__ = ['Equation', 'Variable', 'one_in_n', 'TruthTable']
+__all__ = ['Equation', 'Variable', 'one_in_n', 'TruthTable', 'shift_affected_qubits']
