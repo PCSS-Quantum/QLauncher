88e3c630df620301abebc0202d3b34bacf8e73e2
Dawid64 <dawid.siera@gmail.com>
2025-02-27 14:36:48 +0100

    Updated translation test

--- tests/launcher/test_translation.py
+++ tests/launcher/test_translation.py
@@ -8,11 +8,16 @@ def test_qiskit_to_cirq_translation():
     circuit.h(0)
     circuit.cx(0, 1)
     circuit.x(0)
-    translator_to = QiskitTranslation()
-    translator_from = CirqTranslation()
-    qasm = translator_to.to_qasm(circuit)
-    cirq_circuit = translator_from.from_qasm(qasm)
+    translator_qiskit = QiskitTranslation()
+    translator_cirq = CirqTranslation()
+    qasm = translator_qiskit.to_qasm(circuit)
+    cirq_circuit = translator_cirq.from_qasm(qasm)
     assert isinstance(cirq_circuit, cirq.Circuit)
+    cirq_qasm = translator_cirq.to_qasm(cirq_circuit)
+    assert isinstance(cirq_qasm, str)
+    qiskit_circuit = translator_qiskit.from_qasm(cirq_qasm)
+    assert isinstance(qiskit_circuit, qiskit.QuantumCircuit)
+    assert circuit.decompose(reps=5) == qiskit_circuit.decompose(reps=5)
 
 
 def test_auto_translation():
