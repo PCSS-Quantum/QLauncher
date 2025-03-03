9a7a532da28fc0e71c021e8c49bb768a286b5a6b
Dawid64 <dawid.siera@gmail.com>
2025-03-03 16:15:20 +0100

    Removed problem from init and added networkx to requirements

--- quantum_launcher/problems/__init__.py
+++ quantum_launcher/problems/__init__.py
@@ -1,5 +1,5 @@
 """ All problems together """
 from . import problem_formulations
-from .problem_initialization import Raw, MaxCut, EC, QATM, JSSP, TSP, GraphColoring, Problem
+from .problem_initialization import Raw, MaxCut, EC, QATM, JSSP, TSP, GraphColoring
 
-__all__ = ['Raw', 'MaxCut', 'EC', 'QATM', 'JSSP', 'TSP', 'GraphColoring', 'Problem']
+__all__ = ['Raw', 'MaxCut', 'EC', 'QATM', 'JSSP', 'TSP', 'GraphColoring']
