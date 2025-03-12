378649c74871145951a3565a8414e82c6a13a944
Dawid64 <dawid.siera@gmail.com>
2025-03-12 12:31:17 +0100

    Added dependency error

--- quantum_launcher/problems/problem_formulations/__init__.py
+++ quantum_launcher/problems/problem_formulations/__init__.py
@@ -1,13 +1,6 @@
 """ Import managing for problem formulations (attempt to import as much as possible). """
-try:
-    from .bqm import *
-except ImportError:
-    pass
-
-try:
-    from .qubo import *
-except ImportError:
-    pass
+from .bqm import *
+from .qubo import *
 
 try:
     from .hamiltonian import *
