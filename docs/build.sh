734926eef583b530e4ff45056464c7da9c105873
Jakub Jagła <jjagla@man.poznan.pl>
2025-04-25 07:50:40 +0000

    Apply 2 suggestion(s) to 2 file(s)
    Co-authored-by: Dawid Siera <dsiera@man.poznan.pl>

--- docs/build.sh
+++ docs/build.sh
@@ -6,7 +6,7 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR
 
 mkdir -p tutorials
-cp ./../examples/*.ipynb ./tutorials
+cp ../examples/*.ipynb tutorials
 
 make clean
 make html
