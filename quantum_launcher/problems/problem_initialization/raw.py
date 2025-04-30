1a6c860b11c7b170ee5b0abf687361995785634a
Dawid64 <dawid.siera@gmail.com>
2025-04-30 14:40:13 +0200

    added raw

--- quantum_launcher/problems/problem_initialization/raw.py
+++ quantum_launcher/problems/problem_initialization/raw.py
@@ -1,23 +1,52 @@
 """ This module contains the Raw class."""
+from typing import Any
+
+import numpy as np
+from qiskit.quantum_info import SparsePauliOp
+
 from quantum_launcher.base import Problem, formatter
 
 
 class Raw(Problem):
-    """
-    Class for solving problem implemented in raw mathematical form.
+    """ Meta class for raw problem """
+    __cached_classes__: dict[str, type] = {}
 
-    "Raw mathematical form" means that the problem is defined in format 
-    that can be directly read by the quantum algorithm, such as Qubo, Hamiltonian, etc.
+    @staticmethod
+    def _cache_class(problem_type: str):
+        if problem_type in Raw.__cached_classes__:
+            return Raw.__cached_classes__[problem_type]
 
-    The object contains an instance of the problem written in mentioned raw mathematical form,
-    can be passed into Quantum Launcher.
+        class _Raw(Raw):
+            pass
 
-    Attributes:
-        instance (any): Formulated problem instance.
-    """
+        formatter(_Raw, problem_type)(_raw_formatter)
+        Raw.__cached_classes__[problem_type] = _Raw
+        return _Raw
 
-    def __init__(self, instance: any = None, instance_name: str | None = None) -> None:
-        super().__init__(instance=instance, instance_name=instance_name)
+    @staticmethod
+    def _auto_map_problem(obj: Any) -> str:
+        """Automatically maps obj into str.
 
-    def _get_path(self) -> str:
-        return f'{self.name}/{self.instance_name}'
+        Args:
+            obj (Any): obj of some problem class.
+
+        Returns:
+            str: name of problem class that obj is in.
+        """
+        if isinstance(obj, SparsePauliOp):
+            return 'hamiltonian'
+        if isinstance(obj, tuple) and len(obj) == 2 and \
+                isinstance(obj[0], np.ndarray) and isinstance(obj[1], (int, float)):
+            return 'qubo'
+        raise ValueError
+
+    def __new__(cls, obj: Any, instance_name: str = 'Raw'):
+        if cls is not Raw:
+            return super().__new__(cls)
+        problem_type = cls._auto_map_problem(obj)
+        true_cls = cls._cache_class(problem_type)
+        return true_cls(obj, instance_name)
+
+
+def _raw_formatter(raw: Raw) -> Any:
+    return raw.instance
