c77fd6ff00a0470ee507ad0e33ab9c1e03b5ec16
Dawid64 <dawid.siera@gmail.com>
2025-08-13 11:56:33 +0200

    Updated BBS algorithm

--- qlauncher/routines/orca/backends.py
+++ qlauncher/routines/orca/backends.py
@@ -2,7 +2,7 @@ from qlauncher.base import Backend
 from qlauncher.exceptions import DependencyError
 
 try:
-    from ptseries.algorithms.binary_solvers import BinaryBosonicSolver
+    from ptseries.tbi import create_tbi
 except ImportError as e:
     raise DependencyError(e, install_hint='orca', private=True) from e
 
@@ -13,19 +13,10 @@ class OrcaBackend(Backend):
     def __init__(self, name: str) -> None:
         super().__init__(name)
 
-    def get_bbs(
-        self,
-        pb_dim: int,
-        objective,
-        input_state,
-        **kwargs
-    ) -> BinaryBosonicSolver:
-        return BinaryBosonicSolver(
-            pb_dim=pb_dim,
-            objective=objective,
-            input_state=input_state,
-            **kwargs
-        )
+    def get_tbi(self):
+        """Returns tbi
+        """
+        return create_tbi()
 
     def get_args(self):
         return {}
