I hate qiskit sometimes - Fix broadcasting of params and observables in EstimatorV1ToV2 adapter

qlauncher/routines/qiskit/adapters.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from collections.abc import Sequence
 from typing import Any, Iterable
 
@@ -192,16 +193,23 @@ class EstimatorV1ToEstimatorV2Adapter(BaseEstimatorV2):
     def _run(self, pubs: Iterable[EstimatorPub]) -> PrimitiveResult[PubResult]:
         results = []
         for pub in pubs:
-            obs_list = pub.observables.tolist()
-            if isinstance(obs_list, dict):  # yep
-                obs_list = [obs_list.items()]
-            else:
-                obs_list = [e.items() for e in obs_list]
+            observables = pub.observables
+            parameter_values = pub.parameter_values
+
+            param_shape = parameter_values.shape
+            param_indices = np.fromiter(np.ndindex(param_shape), dtype=object).reshape(param_shape)
+            bc_param_ind, bc_obs = np.broadcast_arrays(param_indices, observables)
+
+            params_final, obs_final = [], []
+            for index in np.ndindex(*bc_param_ind.shape):
+                param_index = bc_param_ind[index]
+                params_final.append(parameter_values[param_index].as_array())
+                obs_final.append(bc_obs[index])
 
             res = self.estimator.run(
-                [pub.circuit]*len(obs_list),
-                [SparsePauliOp.from_list(ol) for ol in obs_list],
-                [pub.parameter_values.as_array().flatten()]*len(obs_list)
+                [pub.circuit]*len(params_final),
+                [SparsePauliOp.from_list(o.items()) for o in obs_final],
+                params_final
             ).result()
             results.append(res)
 

tests/launcher/test_routines.py
@@ -31,32 +31,21 @@ def test_v2_estimator_adapter():
     estimator_v1_adapted = EstimatorV1ToEstimatorV2Adapter(estimator_v1)
     estimator_v2 = EstimatorV2()
 
-    v2_result = estimator_v2.run([(circ, [obs.hamiltonian, obs2.hamiltonian], [1., 2.])]).result()
-    v1_adapted_result = estimator_v1_adapted.run([(circ, [obs.hamiltonian, obs2.hamiltonian], [1., 2.])]).result()
+    for inpt in [
+        [(circ, [obs.hamiltonian, obs2.hamiltonian], [1., 2.])],
+        [(circ, obs2.hamiltonian, [1., 2.])],
+        [(circ, obs.hamiltonian, [1., 2.]), (circ, obs2.hamiltonian, [1., 2.])],
+        [(circ, obs.hamiltonian, [[1., 2.], [2., 3.]]), (circ, obs2.hamiltonian, [[1., 2.], [2., 3.]])],
+        [(circ, [obs.hamiltonian, obs2.hamiltonian], [[1., 2.], [2., 3.]])],
+    ]:
+        v2_result = estimator_v2.run(inpt).result()
+        v1_adapted_result = estimator_v1_adapted.run(inpt).result()
 
-    assert len(v2_result) == len(v1_adapted_result)
+        assert len(v2_result) == len(v1_adapted_result)
 
-    for r1, r2 in zip(v1_adapted_result, v2_result):
-        assert np.allclose(r1.data.evs, r2.data.evs, atol=0.05)
-        assert np.allclose(r1.data.stds, r2.data.stds, atol=0.05)
-
-    v2_result = estimator_v2.run([(circ, obs2.hamiltonian, [1., 2.])]).result()
-    v1_adapted_result = estimator_v1_adapted.run([(circ, obs2.hamiltonian, [1., 2.])]).result()
-
-    assert len(v2_result) == len(v1_adapted_result)
-
-    for r1, r2 in zip(v1_adapted_result, v2_result):
-        assert np.allclose(r1.data.evs, r2.data.evs, atol=0.05)
-        assert np.allclose(r1.data.stds, r2.data.stds, atol=0.05)
-
-    v2_result = estimator_v2.run([(circ, obs.hamiltonian, [1., 2.]), (circ, obs2.hamiltonian, [1., 2.])]).result()
-    v1_adapted_result = estimator_v1_adapted.run([(circ, obs.hamiltonian, [1., 2.]), (circ, obs2.hamiltonian, [1., 2.])]).result()
-
-    assert len(v2_result) == len(v1_adapted_result)
-
-    for r1, r2 in zip(v1_adapted_result, v2_result):
-        assert np.allclose(r1.data.evs, r2.data.evs, atol=0.05)
-        assert np.allclose(r1.data.stds, r2.data.stds, atol=0.05)
+        for r1, r2 in zip(v1_adapted_result, v2_result):
+            assert np.allclose(r1.data.evs, r2.data.evs, atol=0.05)
+            assert np.allclose(r1.data.stds, r2.data.stds, atol=0.05)
 
 
 def test_v2_sampler_adapter():