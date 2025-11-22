10551970e8cbb57b60110fc0e4015b77e1a09f26
Dawid64 <dawid.siera@gmail.com>
2025-11-22 11:05:55 +0100

    Add BFS search for adapters

--- qlauncher/base/problem_like.py
+++ qlauncher/base/problem_like.py
@@ -1,15 +1,31 @@
 from collections.abc import Callable
-from typing import Any
+from typing import TYPE_CHECKING, Any
 
 import numpy as np
+from pyqubo import Spin
 from qiskit.quantum_info import SparsePauliOp
 from qiskit_nature.second_q.drivers import PySCFDriver
 from qiskit_nature.second_q.formats.molecule_info import MoleculeInfo
 from qiskit_nature.second_q.mappers import ParityMapper, QubitMapper
-from qiskit_nature.second_q.problems import ElectronicStructureProblem
+from qiskit_optimization.converters import QuadraticProgramToQubo
+from qiskit_optimization.translators import from_ising
+
+if TYPE_CHECKING:
+	from qiskit_nature.second_q.problems import ElectronicStructureProblem
 
 
-class ProblemLike: ...
+class ProblemLike:
+	_all_problems: dict[str, type['ProblemLike']] = {}
+
+	def __init_subclass__(cls) -> None:
+		if ProblemLike not in cls.__bases__:
+			return
+		ProblemLike._all_problems[cls.__name__] = cls
+		cls._mapping: dict[type[ProblemLike], Callable[[], ProblemLike]] = {}
+		for method_name in cls.__dict__:
+			if method_name.startswith('to_'):
+				method = cls.__dict__[method_name]
+				cls._mapping[method.__annotations__['return']] = method
 
 
 class QUBO(ProblemLike):
@@ -42,6 +58,29 @@ class QUBO(ProblemLike):
 
 		return FN(function)
 
+	def to_bqm(self: 'QUBO') -> 'BQM':
+		matrix = self.matrix
+		symmetric = (self.matrix.transpose() == self.matrix).all()
+		if not symmetric:
+			for i in range(len(self.matrix)):
+				for j in range(len(self.matrix)):
+					if i > j:
+						matrix[i][j] = 0
+
+		values_and_qubits = {(x, y): c for y, r in enumerate(matrix) for x, c in enumerate(r) if c != 0}
+		number_of_qubits = len(matrix)
+		qubits = [Spin(f'x{i}') for i in range(number_of_qubits)]
+		H = 0
+		for (x, y), value in values_and_qubits.items():
+			if symmetric:
+				H += value / len({x, y}) * qubits[x] * qubits[y]
+			else:
+				H += value * qubits[x] * qubits[y]
+		model = H.compile()
+		bqm = model.to_bqm()
+		bqm.offset += self.offset
+		return BQM(bqm, model)
+
 
 class FN(ProblemLike):
 	def __init__(self, function: Callable[[np.ndarray], float]) -> None:
@@ -68,9 +107,15 @@ class Hamiltonian(ProblemLike):
 
 	def get_QAOAAnsatz_initial_state(self) -> None: ...
 
+	def to_qubo(self) -> QUBO:
+		qp = from_ising(self.hamiltonian)
+		conv = QuadraticProgramToQubo()
+		qubo = conv.convert(qp).objective
+		return QUBO(qubo.quadratic.to_array(), 0)
+
 
 class BQM(ProblemLike):
-	def __init__(self, bqm: Any) -> None:  # noqa: ANN401
+	def __init__(self, bqm: Any, model: Any = None) -> None:  # noqa: ANN401
 		self.bqm = bqm
 
 
