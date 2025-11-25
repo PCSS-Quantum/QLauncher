cdcd58e6e5e43480b3fb8e2b3c8ea1194fa4be4b
Dawid64 <dawid.siera@gmail.com>
2025-11-25 16:17:46 +0100

    Add Circuit running support to qlauncher

--- qlauncher/problems/problem_initialization/circuit.py
+++ qlauncher/problems/problem_initialization/circuit.py
@@ -2,10 +2,10 @@
 
 from qiskit.primitives.containers.sampler_pub import SamplerPubLike
 
-from qlauncher.base.base import Problem
+from qlauncher.base import ProblemLike
 
 
-class _Circuit(Problem):
-	def __init__(self, pub: SamplerPubLike, shots: int, instance_name: str = 'unnamed') -> None:
-		super().__init__({'pub': pub, 'shots': shots}, instance_name)
+class _Circuit(ProblemLike):
+	def __init__(self, pub: SamplerPubLike) -> None:
 		self.variant = 'Circuit'
+		self.pub = pub
