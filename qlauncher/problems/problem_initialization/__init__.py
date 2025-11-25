69d5623d4a0818154b839f6d9741d9a26846ad7f
Dawid64 <dawid.siera@gmail.com>
2025-11-25 12:08:46 +0100

    Remove old problems

--- qlauncher/problems/problem_initialization/__init__.py
+++ qlauncher/problems/problem_initialization/__init__.py
@@ -1,12 +1,10 @@
 from .circuit import _Circuit
-from .ec import EC
 from .graph_coloring import GraphColoring
 from .jssp import JSSP
 from .knapsack import Knapsack
-from .maxcut import MaxCut
 from .qatm import QATM
 from .raw import Raw
 from .tabular_ml import TabularML
 from .tsp import TSP
 
-__all__ = ['Raw', 'QATM', 'JSSP', 'EC', 'MaxCut', 'TSP', 'GraphColoring', 'TabularML', 'Knapsack', '_Circuit']
+__all__ = ['Raw', 'QATM', 'JSSP', 'TSP', 'GraphColoring', 'TabularML', 'Knapsack', '_Circuit']
