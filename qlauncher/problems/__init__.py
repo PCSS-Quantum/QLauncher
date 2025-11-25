ede209f7449d7bd270b8cbadcd2f155646473f19
Dawid64 <dawid.siera@gmail.com>
2025-11-25 12:23:16 +0100

    Remove old QATM

--- qlauncher/problems/__init__.py
+++ qlauncher/problems/__init__.py
@@ -5,6 +5,7 @@ from qlauncher.base import Problem
 from . import problem_formulations as _
 from .ec import EC
 from .maxcut import MaxCut
-from .problem_initialization import JSSP, QATM, TSP, GraphColoring, Knapsack, Raw, TabularML, _Circuit
+from .problem_initialization import JSSP, TSP, GraphColoring, Knapsack, Raw, TabularML, _Circuit
+from .qatm import QATM
 
 __all__ = ['Problem', 'Raw', 'MaxCut', 'EC', 'QATM', 'JSSP', 'TSP', 'GraphColoring', 'TabularML', 'Knapsack', '_Circuit']
--- qlauncher/problems/problem_initialization/__init__.py
+++ qlauncher/problems/problem_initialization/__init__.py
@@ -2,9 +2,8 @@ from .circuit import _Circuit
 from .graph_coloring import GraphColoring
 from .jssp import JSSP
 from .knapsack import Knapsack
-from .qatm import QATM
 from .raw import Raw
 from .tabular_ml import TabularML
 from .tsp import TSP
 
-__all__ = ['Raw', 'QATM', 'JSSP', 'TSP', 'GraphColoring', 'TabularML', 'Knapsack', '_Circuit']
+__all__ = ['Raw', 'JSSP', 'TSP', 'GraphColoring', 'TabularML', 'Knapsack', '_Circuit']
