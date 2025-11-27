7be6a834fa878df0e46180affdbe938e5318b2ee
Dawid64 <dawid.siera@gmail.com>
2025-11-27 17:01:28 +0100

    Fix knapsack

--- qlauncher/problems/__init__.py
+++ qlauncher/problems/__init__.py
@@ -5,6 +5,6 @@ from qlauncher.base import Problem
 from .optimization import EC, JSSP, QATM, TSP, GraphColoring, Knapsack, MaxCut
 from .other import TabularML
 
-_Circuit = None
+Raw, _Circuit = [None] * 2
 
 __all__ = ['Problem', 'MaxCut', 'EC', 'QATM', 'JSSP', 'TSP', 'GraphColoring', 'TabularML', 'Knapsack', '_Circuit']
