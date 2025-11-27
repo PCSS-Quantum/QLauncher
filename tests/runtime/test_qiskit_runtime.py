d4eb58b3c88e065c1a59638271430d2fe8b9effa
Dawid64 <dawid.siera@gmail.com>
2025-11-27 13:06:41 +0100

    Minimized tests using pytest parametrization

--- tests/runtime/test_qiskit_runtime.py
+++ tests/runtime/test_qiskit_runtime.py
@@ -1,14 +1,19 @@
 import numpy as np
+import pytest
 from qiskit import QuantumCircuit
 from qiskit.quantum_info import SparsePauliOp
 
 from qlauncher import QLauncher
 from qlauncher.base import Result
 from qlauncher.base.problem_like import Hamiltonian
-from qlauncher.problems import EC, JSSP, QATM, TSP, GraphColoring, Knapsack, MaxCut
 from qlauncher.routines.qiskit import FALQON, QAOA, QiskitBackend  # , AQTBackend
 from qlauncher.routines.qiskit.algorithms.qiskit_native import VQE, Molecule
 from qlauncher.utils import int_to_bitstring
+from tests.runtime.utils import ALL_PROBLEMS, PROBLEM_MAP
+
+
+def _get_hamiltonian() -> Hamiltonian:
+	return Hamiltonian(SparsePauliOp.from_list([('ZZ', -1), ('ZI', 2), ('IZ', 2), ('II', -1)]))
 
 
 def test_int_to_bs() -> None:
@@ -24,20 +29,18 @@ def test_circuit() -> None:
 
 
 def test_falqon() -> None:
-	pr = EC.from_preset(instance_name='micro')
 	qaoa = FALQON(max_reps=1)
 	backend = QiskitBackend('local_simulator')
-	launcher = QLauncher(pr, qaoa, backend)
+	launcher = QLauncher(_get_hamiltonian(), qaoa, backend)
 
 	results = launcher.run()
 	assert isinstance(results, Result)
 
 
 def test_QAOA() -> None:
-	pr = EC.from_preset(instance_name='micro')
 	qaoa = QAOA(p=1)
 	backend = QiskitBackend('local_simulator')
-	launcher = QLauncher(pr, qaoa, backend)
+	launcher = QLauncher(_get_hamiltonian(), qaoa, backend)
 
 	results = launcher.run()
 	assert isinstance(results, Result)
@@ -54,117 +57,14 @@ def test_VQE() -> None:
 
 
 #! We use FALQON for problem tests as it is very fast to execute
-
-
-def test_ec() -> None:
+@pytest.mark.parametrize('problem_name', ALL_PROBLEMS)
+def test_problems(problem_name: str) -> None:
 	"""Testing function for Exact Cover"""
-	pr = EC.from_preset(instance_name='micro')
+	problem = PROBLEM_MAP[problem_name]
 	qaoa = FALQON(max_reps=1)
 	backend = QiskitBackend('local_simulator')
-	launcher = QLauncher(pr, qaoa, backend)
+	launcher = QLauncher(problem, qaoa, backend)
 
 	# results = launcher.process(save_pickle=True, save_txt=True)
-	results = launcher.run()
-	assert isinstance(results, Result)
-
-
-def test_qatm() -> None:
-	"""Testing function for QATM"""
-	pr = QATM.from_file(path='data/qatm/', instance_name='RCP_3.txt')
-	qaoa = FALQON(max_reps=1)
-	backend = QiskitBackend('local_simulator')
-	launcher = QLauncher(pr, qaoa, backend)
-
-	# results = launcher.process(save_pickle=True)
-	results = launcher.run()
-	assert isinstance(results, Result)
-
-
-def test_jssp() -> None:
-	"""Testing function for Job Shop Shedueling Problem"""
-	pr = JSSP.from_preset('default', optimization_problem=True)
-	qaoa = FALQON(max_reps=1)
-	backend = QiskitBackend('local_simulator')
-	launcher = QLauncher(pr, qaoa, backend)
-
-	# results = launcher.process(save_pickle=True)
-	results = launcher.run()
-	assert isinstance(results, Result)
-
-
-def test_maxcut() -> None:
-	"""Testing function for Max Cut"""
-	pr = MaxCut.from_preset(instance_name='default')
-	qaoa = FALQON(max_reps=1)
-	backend = QiskitBackend('local_simulator')
-	launcher = QLauncher(pr, qaoa, backend)
-
-	# results = launcher.process(save_pickle=True)
-	results = launcher.run()
-	assert isinstance(results, Result)
-
-
-def test_maxcut() -> None:
-	"""Testing function for Max Cut"""
-	pr = MaxCut.from_preset(instance_name='default')
-	qaoa = FALQON(max_reps=1)
-	backend = QiskitBackend('local_simulator')
-	launcher = QLauncher(pr, qaoa, backend)
-
-	# results = launcher.process(save_pickle=True)
-	results = launcher.run()
-	assert isinstance(results, Result)
-
-
-def test_hamiltonian() -> None:
-	"""Testing function for Hamiltonian"""
-	hamiltonian = SparsePauliOp.from_list([('ZZ', -1), ('ZI', 2), ('IZ', 2), ('II', -1)])
-	pr = Hamiltonian(hamiltonian)
-	qaoa = FALQON(max_reps=1)
-	backend = QiskitBackend('local_simulator')
-	launcher = QLauncher(pr, qaoa, backend)
-
-	results = launcher.run()
-	assert results is not None
-	bitstring = results.best_bitstring
-	assert bitstring in ['00', '01', '10', '11']
-
-
-def test_tsp() -> None:
-	"""Testing function for TSP"""
-	pr = TSP.generate_tsp_instance(3)  # Smaller sample size for testing
-	qaoa = FALQON(max_reps=1)
-	backend = QiskitBackend('local_simulator')
-	launcher = QLauncher(pr, qaoa, backend)
-
-	results = launcher.run()
-	assert results is not None
-	bitstring = results.best_bitstring
-	assignments = [bitstring[i : i + 3] for i in range(0, len(bitstring), 3)]
-	assert len(assignments) == 3
-
-
-def test_graph_coloring() -> None:
-	"""Testing function for Graph Coloring"""
-	gc = GraphColoring.from_preset('small')
-	num_colors = gc.num_colors
-	color_bit_length = int(np.ceil(np.log2(num_colors)))
-	qaoa = FALQON(max_reps=1)
-	backend = QiskitBackend('local_simulator')
-	launcher = QLauncher(gc, qaoa, backend)
-	inform = launcher.run()
-	assert isinstance(inform, Result)
-	bitstring = inform.best_bitstring
-	num_qubits = len(bitstring)
-	assert num_qubits == gc.instance.number_of_nodes() * color_bit_length, 'error in encoding, solution contains wrong number of qubits'
-
-
-def test_knapsack() -> None:
-	"""Testing function for Knapsack problem"""
-	pr = Knapsack.from_preset('default')
-	qaoa = FALQON(max_reps=1)
-	backend = QiskitBackend('local_simulator')
-	launcher = QLauncher(pr, qaoa, backend)
-
 	results = launcher.run()
 	assert isinstance(results, Result)
