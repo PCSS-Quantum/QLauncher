6d3193ed1440c75c45714fd363edfad5e61eee02
Anatol Kaczmarek <92335424+Tole-k@users.noreply.github.com>
2025-11-27 16:28:33 +0100

    change to proper use of hampy

--- qlauncher/problems/optimization/jssp_utils/qiskit_scheduler.py
+++ qlauncher/problems/optimization/jssp_utils/qiskit_scheduler.py
@@ -9,27 +9,28 @@ from .scheduler import JobShopScheduler, Task
 
 
 class HamPyScheduler(JobShopScheduler):
+	equation: hampy.Equation
+	onehot: Literal['exact', 'quadratic']
+
 	def __init__(self, job_dict: dict, max_time: int | None = None, onehot: Literal['exact', 'quadratic'] = 'exact'):
 		super().__init__(job_dict, max_time)
 		self.equation = hampy.Equation(self.n)
 		self.onehot = onehot
-		self.H = 0
 
 	def _get_variable(self, task: Task, time: int) -> Variable:
 		return self.equation[self.assignment_index[(task, time)]]
 
 	def _add_expression(self, var1: Variable, var2: Variable, lagrange_factor: float) -> None:
-		self.H += lagrange_factor * (var1 & var2).hamiltonian
+		self.equation += lagrange_factor * (var1 & var2)
 
 	def _add_expression_one_start(self, variables: list[int | Variable], lagrange_factor: float) -> None:
 		if self.onehot == 'exact':
-			self.H += lagrange_factor * (~hampy.one_in_n(variables, self.n)).hamiltonian
+			self.equation += lagrange_factor * (~hampy.one_in_n(variables, self.n))
 		elif self.onehot == 'quadratic':
-			self.H += lagrange_factor * hampy.one_in_n(variables, self.n, quadratic=True).hamiltonian
+			self.equation += lagrange_factor * hampy.one_in_n(variables, self.n, quadratic=True)
 
 	def _add_variable(self, var: Variable, bias: float) -> None:
-		self.H += var.to_equation().hamiltonian * bias
+		self.equation += var.to_equation() * bias
 
 	def _get_final(self) -> SparsePauliOp:
-		assert isinstance(self.H, SparsePauliOp)
-		return self.H.simplify().copy()
+		return self.equation.hamiltonian.copy()
