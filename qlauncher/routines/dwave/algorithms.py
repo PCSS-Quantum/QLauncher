d4f4053723db30fdb8b33592272a4528494fded8
Dawid64 <dawid.siera@gmail.com>
2025-11-27 13:05:49 +0100

    Changed D-wave algorithm structure

--- qlauncher/routines/dwave/algorithms.py
+++ qlauncher/routines/dwave/algorithms.py
@@ -1,29 +1,37 @@
 """DWave algorithms"""
 
+from abc import ABC, abstractmethod
+from typing import Any
+
 from qlauncher.base import Algorithm, Result
 from qlauncher.base.problem_like import BQM
 from qlauncher.exceptions import DependencyError
 from qlauncher.routines.dwave.backends import BQMBackend
 
 try:
-	from dimod import SampleSet
+	from dimod import Sampler, SampleSet
+	from dwave.samplers import SimulatedAnnealingSampler, SteepestDescentSampler, TabuSampler
 except ImportError as e:
 	raise DependencyError(e, install_hint='dwave') from e
 
 
-class DwaveSolver(Algorithm[BQM, BQMBackend]):
-	def __init__(self, chain_strength=1, num_reads=1000, **alg_kwargs) -> None:
+class DwaveSolver(Algorithm[BQM, BQMBackend], ABC):
+	def __init__(self, chain_strength: int = 1, num_reads: int = 1000, **alg_kwargs) -> None:
 		self.chain_strength = chain_strength
 		self.num_reads = num_reads
 		self.label: str = 'TBD_TBD'
 		super().__init__(**alg_kwargs)
 
 	def run(self, problem: BQM, backend: BQMBackend) -> Result:
-		res = self._solve_bqm(problem.bqm, backend.sampler, **self.alg_kwargs)
+		res = self._solve_bqm(problem.bqm, self._get_sampler(), **self.alg_kwargs)
 		return self._construct_result(res)
 
-	def _solve_bqm(self, bqm, sampler, **kwargs):
+	def _solve_bqm(self, bqm: Any, sampler: Sampler, **kwargs) -> SampleSet:
 		return sampler.sample(bqm, num_reads=self.num_reads, label=self.label, chain_strength=self.chain_strength, **kwargs)
+
+	@abstractmethod
+	def _get_sampler(self) -> Sampler:
+		pass
 
 	def _construct_result(self, result: SampleSet) -> Result:
 		distribution = {}
@@ -37,3 +45,24 @@ class DwaveSolver(Algorithm[BQM, BQMBackend]):
 			energies[bitstring] = energy
 
 		return Result.from_counts_energies(distribution, energies, result)
+
+
+class Tabu(DwaveSolver):
+	"""Tabu search simulator backend"""
+
+	def _get_sampler(self) -> Sampler:
+		return TabuSampler()
+
+
+class SimulatedAnnealing(DwaveSolver):
+	"""Simulated annealing simulator backend"""
+
+	def _get_sampler(self) -> Sampler:
+		return SimulatedAnnealingSampler()
+
+
+class SteepestDescent(DwaveSolver):
+	"""Steepest descent simulator backend"""
+
+	def _get_sampler(self) -> Sampler:
+		return SteepestDescentSampler()
