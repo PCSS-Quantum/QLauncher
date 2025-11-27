8243a89872906653fede994bbcee8cb04186657f
Dawid64 <dawid.siera@gmail.com>
2025-11-27 16:59:28 +0100

    Remove old code

--- qlauncher/problems/problem_initialization/__init__.py
+++
@@ -1,8 +0,0 @@
-from ..circuit import _Circuit
-from .graph_coloring import GraphColoring
-from .knapsack import Knapsack
-from .raw import Raw
-from .tabular_ml import TabularML
-from .tsp import TSP
-
-__all__ = ['Raw', 'TSP', 'GraphColoring', 'TabularML', 'Knapsack', '_Circuit']
