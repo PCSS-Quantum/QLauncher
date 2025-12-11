12d99668efc2d586661d183586bb8cb8552af6da
Jakub Jagła <jjagla@man.poznan.pl>
2025-12-11 13:46:44 +0100

    Add error mitigation to qlauncher qiskit & cirq backends

---
+++ qlauncher/routines/qiskit/mitigation_suppression/base.py
@@ -0,0 +1,48 @@
+from abc import ABC, abstractmethod
+from qiskit import QuantumCircuit
+from qiskit.quantum_info import SparsePauliOp
+
+from qlauncher.base import Result
+# from qlauncher.routines.cirq import CirqBackend
+
+
+class CircuitModificationMethod(ABC):
+	"""Method that relies only on modifying the input circuit"""
+
+	@abstractmethod
+	def modify_circuit(self, circuit: QuantumCircuit) -> QuantumCircuit:
+		pass
+
+
+class CircuitExecutionMethod(ABC):
+	"""Method that relies on executing multiple modified versions of the circuit"""
+
+	@abstractmethod
+	def sample(self, circuit: QuantumCircuit, backend: 'QiskitBackend', shots: int = 1024) -> dict[str, int]:
+		"""
+		Sample circuit on the backend.
+
+		Args:
+		    circuit (QuantumCircuit): Circuit to run.
+		    backend (QiskitBackend): Backend to run on.
+		    shots (int, optional): Number of samples to collect. Defaults to 1024.
+
+		Returns:
+		    dict[str,int]: Bitstring counts.
+		"""
+		pass
+
+	@abstractmethod
+	def estimate(self, circuit: QuantumCircuit, observable: SparsePauliOp, backend: 'QiskitBackend') -> float:
+		"""
+		Estimate energy of observable after running a given circuit on the backend.
+
+		Args:
+		    circuit (QuantumCircuit): Circuit to run.
+		    observable (SparsePauliOp): Observable to estimate.
+		    backend (QiskitBackend): Backend to use.
+
+		Returns:
+		    float: Estimated energy of the observable.
+		"""
+		pass
--- tests/runtime/utils.py
+++ tests/runtime/utils.py
@@ -1,4 +1,5 @@
 from qlauncher.problems import EC, JSSP, QATM, TSP, GraphColoring, Knapsack, MaxCut
+from qlauncher.routines.qiskit.mitigation_suppression.mitigation import PauliTwirling, NoMitigation, ZeroNoiseExtrapolation
 
 PROBLEM_MAP = {
 	'ec': EC.from_preset('micro'),
@@ -7,7 +8,15 @@ PROBLEM_MAP = {
 	'tsp': TSP.generate_tsp_instance(3),
 	'gc': GraphColoring.from_preset('small'),
 	'knapsack': Knapsack.from_preset('small'),
-	'JSSP':JSSP.from_preset('default')
+	'JSSP': JSSP.from_preset('default'),
 }
 
 ALL_PROBLEMS = list(PROBLEM_MAP.keys())
+
+MITIGATION_MAP = {
+	'zne': ZeroNoiseExtrapolation(),
+	'pauli-twirl': PauliTwirling(4),
+	'none': NoMitigation(),
+}
+
+ALL_MITIGATION_STRATEGIES = list(MITIGATION_MAP.keys())
