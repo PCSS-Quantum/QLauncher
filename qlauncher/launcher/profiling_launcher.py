4f5abdc94a08f47759699c078705e9a61d3ee107
Dawid64 <dawid.siera@gmail.com>
2025-12-18 11:11:14 +0100

    initial profiling idea

--- qlauncher/launcher/profiling_launcher.py
+++ qlauncher/launcher/profiling_launcher.py
@@ -1,7 +1,9 @@
 import cProfile
 import pstats
-
-from qlauncher.base import Algorithm, Backend, Problem, Result
+import subprocess
+import sys
+import tempfile
+from pathlib import Path
 
 from .qlauncher import QLauncher
 
@@ -10,20 +12,38 @@ class ProfilingLauncher(QLauncher):
 	"""Launcher made for debugging purposes of algorithms and other launchers focusing on performance issues
 
 	Attributes:
-	    profiler_path (str) path where to save the profiling results.
+		profiler_path (str) path where to save the profiling results.
 
 	"""
 
-	def __init__(self, problem: Problem, algorithm: Algorithm, backend: Backend, profiler_path: str = 'profiling-results.prof'):
-		super().__init__(problem, algorithm, backend)
-		self._profiler_path = profiler_path
+	def profile(
+		self,
+		profiler_path: str | Path | None,
+		problem_search: bool = True,
+		algorithm: bool = True,
+	) -> pstats.Stats:
+		if not problem_search:
+			compatible_problem = self._get_compatible_problem()
 
-	def run(self) -> Result:
 		with cProfile.Profile() as pr:
-			result = super().run()
-		stats = pstats.Stats(pr)
-		stats.sort_stats(pstats.SortKey.TIME)
-		stats.reverse_order()
-		stats.print_stats()
-		stats.dump_stats(self._profiler_path)
-		return result
+			if problem_search:
+				compatible_problem = self._get_compatible_problem()
+			if algorithm:
+				# compatible_problem is bounded as both scenarios have been covered
+				self.algorithm.run(compatible_problem, self.backend)  # type: ignore
+
+		stats = pstats.Stats(pr).sort_stats(pstats.SortKey.CUMULATIVE).reverse_order()
+		if profiler_path is not None:
+			stats.dump_stats(profiler_path)
+		return stats
+
+	def profile_with_visualization(self, stats: pstats.Stats) -> None:
+		try:
+			import snakeviz  # type: ignore # noqa: F401
+		except ModuleNotFoundError as e:
+			raise ModuleNotFoundError(
+				'To use `ProfilingLauncher.profile_with_visualization` you need to instal snakeviz\n   Use `pip install snakeviz`'
+			) from e
+		with tempfile.NamedTemporaryFile() as tf:
+			stats.dump_stats(tf.name)
+			subprocess.run([sys.executable, '-m', 'snakeviz', tf.name])
